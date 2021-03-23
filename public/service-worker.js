9e6ef10c3e418a25cf15aa73ddd61bccf59f2ccd
Pete-Scale <petescale@gmail.com>
2021-03-22 21:01:06 -0500

    service worker code, manifest rearranged, index script service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,64 @@
 console.log("Service Worker Works!")
+
+const FILES_TO_CACHE = [
+    '/',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/index.html',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/style.css',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+];
+
+const CACHE_NAME = 'budget-tracker-cache-v1';
+const DATA_CACHE_NAME = 'bt-data-cache-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  const currentCaches = [CACHE_NAME, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
